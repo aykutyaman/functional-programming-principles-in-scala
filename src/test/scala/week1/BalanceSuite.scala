d00a0beba7a8fbfb65403bbf3f4667cd43bb24c2
Aykut Yaman <yaman.ayk@gmail.com>
2018-10-12 06:24:17 +0300

    week 1 programming assignment

---
+++ src/test/scala/week1/BalanceSuite.scala
@@ -0,0 +1,28 @@
+package week1
+
+import org.scalatest.FunSuite
+
+class BalanceSuite extends FunSuite {
+  import Main.balance
+
+  test("balance with no parenthesis") {
+    assert(balance("".toList))
+  }
+
+  test("balance: '(if (zero? x) max (/ 1 x))' is balanced") {
+    assert(balance("(if (zero? x) max (/ 1 x))".toList))
+  }
+
+  test("balance: 'I told him ...' is balanced") {
+    assert(balance("I told him (that it's not (yet) done).\n(But he wasn't listening)".toList))
+  }
+
+  test("balance: ':-)' is unbalanced") {
+    assert(!balance(":-)".toList))
+  }
+
+  test("balance: counting is not enough") {
+    assert(!balance("())(".toList))
+  }
+
+}
---
+++ src/test/scala/week1/PascalSuite.scala
@@ -0,0 +1,23 @@
+package week1
+
+import org.scalatest.FunSuite
+
+class PascalSuite extends FunSuite {
+  import Main.pascal
+  test("pascal: col=0,row=2") {
+    assert(pascal(0,2) === 1)
+  }
+
+  test("pascal: col=1,row=2") {
+    assert(pascal(1,2) === 2)
+  }
+
+  test("pascal: col=1,row=3") {
+    assert(pascal(1,3) === 3)
+  }
+
+  test("pascal: col=2,row=5") {
+    assert(pascal(2,5) === 10)
+  }
+
+}
